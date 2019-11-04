d148660e3ab869d4f601eaaa0c1835dfef70d1c3
Abhinav Krishna C K <me@abhy.me>
2019-11-04 21:43:42 +0530

    fossnssify gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `FOSS NSS| Free and Open Source Software Cell of NSSCE`,
+    description: `Free and Open Source Software Cell of NSS College of Engineering, Palakkad`,
+    author: `@fossadmin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/foss-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
