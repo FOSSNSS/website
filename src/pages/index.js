51feeb861c259bb55110c5c093b4c6c59c3d96f0
Denil Verghese <denilcv@gmail.com>
2019-11-05 15:33:08 +0530

    Minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,11 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to FOSS NSS</p>
-    <p>Now go build something great.</p>
+    <p>This is the official site of FOSSCell of NSS COllege of Engineering,Palakkad.</p>
+    <p>This site is preparing for something :)</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/index2/">Development Page</Link>
   </div>
 )
 
--- src/pages/index2.js
+++ src/pages/index2.js
@@ -5,12 +5,12 @@ import { Link } from "gatsby"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
-  <template>
+  <div>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </template>
+  </div>
 )
 
 export default SecondPage
