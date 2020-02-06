8689d7e7aecc605090780baa0b5e114009bebbb1
Abhinav Krishna C K <me@abhy.me>
2020-02-06 19:25:38 +0530

    Add some more links to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,22 @@ const Footer = () => (
     <footer>
 		<div className="topfooter">
 			<div className="sect1">
-
+                <h2>Navigation</h2>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="https://forms.gle/sWCU2L2KYpGmTZzg8">Become a member</a></li>
+                    <li><a href="https://loomio.org">Loomio Discussions Group</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
 			</div>
 			<div className="sect2">
-				
+                <h2>Links</h2>
+				<ul>
+                    <li><a href="https://plus.fosscommunity.in/">PLUS - Palakkad</a></li>
+                    <li><a href="https://wikiwikiweb.de/LugsList/">GLUG/FSUG India lists</a></li>
+                    <li><a href="https://www.gnu.org/">GNU Project</a></li>
+                    <li><a href="https://gitlab.com/fossnss/website">Website Source</a></li>
+                </ul>
 			</div>
 		</div>
         <div className='footer'>
