68d2e3942fd45d34082b09b4a5641e6c44a3b19d
hazilMohamed <mohamedhazil14@yahoo.in>
2020-02-23 22:14:24 +0530

    created dropdown on desktop view, need to work on mobile view and style all the nested list

--- src/components/navlinks.js
+++ src/components/navlinks.js
@@ -1,41 +1,92 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import '../styles/partials/layouts/_navlinks.scss';
 
-const NavLinks = () => (
-    <ul className='nav-links'>
+const NavLinks = () => {
+    const [active, setActive] = React.useState(false);
+    function changeState() {
+        if (active) {
+            setActive(false);
+        } else {
+            setActive(true);
+        }
+    }
+    return (
+        <ul className='nav-links'>
+            <li>
+                <Link to='/' activeClassName='active-link'>
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to='/blog'
+                    activeClassName='active-link'
+                    partiallyActive={true}>
+                    Blog
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to='/events'
+                    activeClassName='active-link'
+                    partiallyActive={true}>
+                    Events
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to='/members'
+                    activeClassName='active-link'
+                    partiallyActive={true}>
+                    Troop
+                </Link>
+            </li>
+            <button className='accordion' onClick={changeState}>
+                More
+            </button>
+            <NestedList
+                activateStatus={
+                    active ? 'nestedList activate-nestedList' : 'nestedList'
+                }
+            />
+        </ul>
+    );
+};
+
+const NestedList = props => (
+    <ul className={props.activateStatus}>
         <li>
-            <Link to='/' activeClassName='active-link'>
-                Home
-            </Link>
+            <p>
+                <Link
+                    to='/about'
+                    activeClassName='mobactive-link'
+                    partiallyActive={true}
+                    onClick={props.action}>
+                    About
+                </Link>
+            </p>
         </li>
         <li>
-            <Link
-                to='/blog'
-                activeClassName='active-link'
-                partiallyActive={true}>
-                Blog
-            </Link>
+            <p>
+                <Link
+                    to='/acheivements'
+                    activeClassName='mobactive-link'
+                    partiallyActive={true}
+                    onClick={props.action}>
+                    Acheivements
+                </Link>
+            </p>
         </li>
         <li>
-            <Link
-                to='/events'
-                activeClassName='active-link'
-                partiallyActive={true}>
-                Events
-            </Link>
-        </li>
-        <li>
-            <Link
-                to='/members'
-                activeClassName='active-link'
-                partiallyActive={true}>
-                Troop
-            </Link>
-        </li>
-        <li>
-            <Link to='/about' activeClassName='active-link'>
-                About
-            </Link>
+            <p>
+                <Link
+                    to='/news'
+                    activeClassName='mobactive-link'
+                    partiallyActive={true}>
+                    News
+                </Link>
+            </p>
         </li>
     </ul>
 );
