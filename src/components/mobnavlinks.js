c0abcb01677c510063199aa57f8aa32753af63e2
hazilMohamed <mohamedhazil14@yahoo.in>
2020-02-24 22:23:15 +0530

    added sub-dropdown to mobile view

--- src/components/mobnavlinks.js
+++ src/components/mobnavlinks.js
@@ -1,54 +1,102 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-const MobNavLinks = props => (
+const MobNavLinks = props => {
+    const [active, setActive] = React.useState(false);
+    function changeState() {
+        if (!active) setActive(true);
+        else setActive(false);
+    }
+    return (
+        <ul className={props.activateStatus}>
+            <li onClick={props.action}>
+                <p>
+                    <Link to='/' activeClassName='mobactive-link'>
+                        Home
+                    </Link>
+                </p>
+            </li>
+            <li>
+                <p>
+                    <Link
+                        to='/blog'
+                        activeClassName='mobactive-link'
+                        partiallyActive={true}
+                        onClick={props.action}>
+                        Blog
+                    </Link>
+                </p>
+            </li>
+            <li>
+                <p>
+                    <Link
+                        to='/events'
+                        activeClassName='mobactive-link'
+                        partiallyActive={true}
+                        onClick={props.action}>
+                        Events
+                    </Link>
+                </p>
+            </li>
+            <li>
+                <p>
+                    <Link
+                        to='/members'
+                        activeClassName='mobactive-link'
+                        partiallyActive={true}>
+                        Troop
+                    </Link>
+                </p>
+            </li>
+            <div>
+                <button
+                    className={
+                        active ? 'accordion activate-accordion' : 'accordion'
+                    }
+                    onClick={changeState}>
+                    More
+                </button>
+                <NestedList
+                    activateStatus={
+                        active ? 'nestedList activate-nestedList' : 'nestedList'
+                    }
+                />
+            </div>
+        </ul>
+    );
+};
+
+const NestedList = props => (
     <ul className={props.activateStatus}>
-        <li onClick={props.action}>
-            <p>
-                <Link to='/' activeClassName='mobactive-link'>
-                    Home
-                </Link>
-            </p>
-        </li>
         <li>
-            <p>
-                <Link
-                    to='/blog'
-                    activeClassName='mobactive-link'
-                    partiallyActive={true}
-                    onClick={props.action}>
-                    Blog
-                </Link>
-            </p>
-        </li>
-        <li>
-            <p>
-                <Link
-                    to='/events'
-                    activeClassName='mobactive-link'
-                    partiallyActive={true}
-                    onClick={props.action}>
-                    Events
-                </Link>
-            </p>
-        </li>
-        <li>
-            <p>
-                <Link
-                    to='/members'
-                    activeClassName='mobactive-link'
-                    partiallyActive={true}>
-                    Troop
-                </Link>
-            </p>
-        </li>
-		<li>
             <p>
                 <Link
                     to='/about'
                     activeClassName='mobactive-link'
-                    partiallyActive={true}>
+                    partiallyActive={true}
+                    onClick={props.action}>
                     About
+                </Link>
+            </p>
+        </li>
+        <li>
+            <p>
+                <Link
+                    to='/acheivements'
+                    activeClassName='mobactive-link'
+                    partiallyActive={true}
+                    onClick={props.action}>
+                    Acheivements
+                </Link>
+            </p>
+        </li>
+        <li>
+            <p>
+                <Link
+                    to='/news'
+                    activeClassName='mobactive-link'
+                    partiallyActive={true}>
+                    News
                 </Link>
             </p>
         </li>
