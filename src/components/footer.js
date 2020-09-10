283ede32c582638856fdd9eb982a7eed9eb49138
hazilMohamed <mohamedhazil14@yahoo.in>
2020-09-10 21:40:08 +0530

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,28 +3,62 @@ import React from 'react';
 
 const Footer = () => (
     <footer>
-		<div className="topfooter">
-			<div className="sect1">
+        <div className='topfooter'>
+            <div className='sect1'>
                 <h2>Navigation</h2>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="https://forms.gle/sWCU2L2KYpGmTZzg8">Become a member</a></li>
-                    <li><a href="https://loomio.org">Loomio Discussions Group</a></li>
-                    <li><a href="/contact">Contact Us</a></li>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                    <li>
+                        <a href='https://forms.gle/sWCU2L2KYpGmTZzg8'>
+                            Become a member
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://loomio.org'>
+                            Loomio Discussions Group
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/contact'>Contact Us</a>
+                    </li>
                 </ul>
-			</div>
-			<div className="sect2">
+            </div>
+            <div className='sect2'>
                 <h2>Links</h2>
-				<ul>
-                    <li><a href="https://plus.fosscommunity.in/">PLUS - Palakkad</a></li>
-                    <li><a href="https://wikiwikiweb.de/LugsList/">GLUG/FSUG India lists</a></li>
-                    <li><a href="https://www.gnu.org/">GNU Project</a></li>
-                    <li><a href="https://gitlab.com/fossnss/website">Website Source</a></li>
+                <ul>
+                    <li>
+                        <a href='https://plus.fosscommunity.in/'>
+                            PLUS - Palakkad
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://wikiwikiweb.de/LugsList/'>
+                            GLUG/FSUG India lists
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://www.gnu.org/'>GNU Project</a>
+                    </li>
+                    <li>
+                        <a href='https://gitlab.com/fossnss/website'>
+                            Website Source
+                        </a>
+                    </li>
                 </ul>
-			</div>
-		</div>
+            </div>
+        </div>
         <div className='footer'>
-            <p>with &hearts; FOSS NSS &#127279; {(new Date().getFullYear())}</p>
+            <p>
+                2020 - <strong>FOSSNSS</strong>
+                <br />
+                Unless otherwise stated, content is available under the
+                <br />
+                <a href='https://creativecommons.org/licenses/by-sa/4.0/'>
+                    Creative Commons Attribution-ShareAlike Licensce
+                </a>
+            </p>
         </div>
     </footer>
 );
